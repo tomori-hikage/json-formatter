059127ff544ae492bfdbcef66256dbb943e8729c
mori_akito <tomori.aki2961@gmail.com>
2018-03-16 16:29:04 +0900

    namespaceの削除

--- json-formatter/Assets/JsonFormatter/Demo/Scripts/Example.cs
+++ json-formatter/Assets/JsonFormatter/Demo/Scripts/Example.cs
@@ -4,53 +4,50 @@ using System.IO;
 using HC.Common;
 
 
-namespace HC
+public class Example : MonoBehaviour
 {
-    public class Example : MonoBehaviour
+    #region イベントメソッド
+
+    private void Start()
     {
-        #region イベントメソッド
+        string json = ReadText("JsonFormatter/Demo/example.json");
 
-        private void Start()
-        {
-            string json = ReadText("JsonFormatter/Demo/example.json");
+        json = JsonFormatter.ToMinifyPrint(json);
+        Debug.Log("MinifyPrint: " + Environment.NewLine + json);
 
-            json = JsonFormatter.ToMinifyPrint(json);
-            Debug.Log("MinifyPrint: " + Environment.NewLine + json);
-
-            json = JsonFormatter.ToPrettyPrint(json, JsonFormatter.IndentType.Space);
-            Debug.Log("PrettyPrint: " + Environment.NewLine + json);
-        }
-
-        #endregion
-
-
-        #region メソッド
-
-        /// <summary>
-        /// テキストをファイルから読み込む
-        /// </summary>
-        /// <param name="path">Application.dataPath以降のファイルパス</param>
-        /// <returns>読み込んだテキスト</returns>
-        public static string ReadText(string path)
-        {
-            string text = string.Empty;
-
-            try
-            {
-                using (var streamReader = new StreamReader(Path.Combine(Application.dataPath, path)))
-                {
-                    text = streamReader.ReadToEnd();
-                    streamReader.Close();
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.LogAssertion(e.Message);
-            }
-
-            return text;
-        }
-
-        #endregion
+        json = JsonFormatter.ToPrettyPrint(json, JsonFormatter.IndentType.Space);
+        Debug.Log("PrettyPrint: " + Environment.NewLine + json);
     }
+
+    #endregion
+
+
+    #region メソッド
+
+    /// <summary>
+    /// テキストをファイルから読み込む
+    /// </summary>
+    /// <param name="path">Application.dataPath以降のファイルパス</param>
+    /// <returns>読み込んだテキスト</returns>
+    public static string ReadText(string path)
+    {
+        string text = string.Empty;
+
+        try
+        {
+            using (var streamReader = new StreamReader(Path.Combine(Application.dataPath, path)))
+            {
+                text = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogAssertion(e.Message);
+        }
+
+        return text;
+    }
+
+    #endregion
 }
